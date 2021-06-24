439f8929429a1284236a55ac18e7610ea0fb48e5
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-23 18:17:54 -0600

    [BB-12] React Boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hero } from '../component/MainPage/Hero';
+
+interface Props {}
+
+export const HomePage = (props: Props) => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
