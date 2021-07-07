8a14dfc38ed10c1aff53f8c05f9e085e4a9290b5
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-06 20:06:12 -0600

    Made resposible welcome page

--- src/component/MainPage/MainPageElements.tsx
+++ src/component/MainPage/MainPageElements.tsx
@@ -4,7 +4,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
   min-height: calc(100vh - 60px);
   display: grid;
-  grid-template-rows: repeat(2, calc((100vh - 60px) / 2));
+  grid-template-rows: 1fr;
+  grid-template-columns: 60vw 40vw;
+
+  @media screen and (max-width: 960px) {
+    grid-template-rows: repeat(2, calc((100vh - 60px) / 2));
+    grid-template-columns: 1fr;
+  }
 
   /* overflow: hidden; */
   /* background: linear-gradient(to left, #0178bd, #368dc5); */
@@ -13,6 +19,7 @@ export const Container = styled.div`
 export const BackgroundWrapper = styled.div`
   height: 100%;
   width: 100%;
+  background-color: #ffebee;
 `;
 
 export const Image = styled.img`
@@ -20,12 +27,17 @@ export const Image = styled.img`
   width: 100%;
   object-fit: cover;
   z-index: 1;
+  clip-path: polygon(0 0%, 100% 0%, 85% 100%, 0% 100%);
+
+  @media screen and (max-width: 960px) {
+    clip-path: polygon(0 0%, 100% 0%, 100% 85%, 0% 100%);
+  }
   /* overflow: hidden; */
   /* transform: scale(1); */
   /* transform-origin: center center 0px; */
 `;
 
-export const Logo = styled.a`
+export const LogoMobile = styled.a`
   img {
     height: 130px;
     width: 130px;
@@ -45,8 +57,30 @@ export const Card = styled.div`
   background: #ffebee;
   display: grid;
   grid-template-columns: 50% 50%;
-  grid-template-rows: 60% 40%;
-  grid-template-areas: 'slogan slogan' 'cakebtn dacquoisebtn';
+  grid-template-rows: 30% 45% 25%;
+  grid-template-areas: 'logo-desktop logo-desktop' 'slogan slogan' 'cakebtn dacquoisebtn';
+  align-items: center;
+
+  @media screen and (max-width: 960px) {
+    grid-template-columns: 50% 50%;
+    grid-template-rows: 60% 40%;
+    grid-template-areas: 'slogan slogan' 'cakebtn dacquoisebtn';
+  }
+`;
+
+export const LogoDesktop = styled.a`
+  grid-area: logo-desktop;
+  display: flex;
+  justify-content: center;
+
+  img {
+    height: 200px;
+    width: 200px;
+    /* transform: translateY(50px); */
+    background: #133853;
+    border-radius: 100px;
+    border: 1px solid #fff;
+  }
 `;
 
 // export const Card = styled.div`
@@ -62,10 +96,16 @@ export const Card = styled.div`
 
 export const Slogan = styled.h2`
   grid-area: slogan;
-  /* font-size: 1.5rem; */
   text-align: left;
   padding: 20px;
   font-family: 'Indie Flower', cursive;
+  font-size: 2rem;
+  line-height: 2;
+
+  @media screen and (max-width: 960px) {
+    font-size: 1.5rem;
+    line-height: 1;
+  }
 `;
 
 const ProductBtn = styled(Link)`
@@ -87,6 +127,7 @@ const ProductBtn = styled(Link)`
   align-items: center;
   transition: all 0.3s ease;
   max-width: 200px;
+  height: 60px;
   font-family: 'Otomanopee One', sans-serif;
 
   &:hover {
--- src/component/MainPage/index.tsx
+++ src/component/MainPage/index.tsx
@@ -4,8 +4,9 @@ import {
   Container,
   BackgroundWrapper,
   Image,
-  Logo,
+  LogoMobile,
   Card,
+  LogoDesktop,
   Slogan,
   CakeBtn,
   DacquoiseBtn,
@@ -19,29 +20,32 @@ export const Welcome = (props: Props) => {
   return (
     <Container>
       <BackgroundWrapper>
-        {isMobile ? (
-          <Image
-            src={require('../../img/welcome_background_mobile.jpg')?.default}
-            alt='mobile_bg'
-          />
-        ) : (
-          <Image
-            src={require('../../img/welcome_background_mobile.jpg')?.default}
-            alt='desktop_bg'
-          />
+        <Image
+          src={require('../../img/welcome_background.jpg')?.default}
+          alt='welcome_background'
+        />
+        {isMobile && (
+          <LogoMobile
+            href='https://www.instagram.com/baking.bunny_yyc/'
+            rel='noreferrer'
+            target='_blank'
+          >
+            <img
+              src={require('../../img/logo_circle.png')?.default}
+              alt='logo_img'
+            />
+          </LogoMobile>
         )}
-        <Logo
-          href='https://www.instagram.com/baking.bunny_yyc/'
-          rel='noreferrer'
-          target='_blank'
-        >
-          <img
-            src={require('../../img/logo_circle.png')?.default}
-            alt='logo_img'
-          />
-        </Logo>
       </BackgroundWrapper>
       <Card>
+        {!isMobile && (
+          <LogoDesktop>
+            <img
+              src={require('../../img/logo_circle.png')?.default}
+              alt='logo_img'
+            />
+          </LogoDesktop>
+        )}
         <Slogan>
           Baking Bunny is specialized in flower cakes, custom designed cakes,
           and dacquoise in birthdays, showers, and every other occaion in
