f965a2db2c23cb3da6bd630424315be81153e3d4
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-13 01:59:39 -0600

    Redesign cart and added initial subtotal component

--- src/component/CartPage/index.tsx
+++ src/component/CartPage/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { products, update } from '../../store/cartSlice';
-import { useAppSelector } from '../../store/hooks';
-import { useAppDispatch } from '../../store/hooks';
-import { remove } from '../../store/cartSlice';
+import React, { useState, useMemo } from 'react';
+import { products, update, remove } from '../../store/cartSlice';
+import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { CartState } from '../../interface/CartState';
 import {
   Container,
@@ -20,12 +18,14 @@ import { Quantity } from './Quantity';
 import { Size } from './Size';
 import { Tastes } from './Tastes';
 import { Price } from './Price';
+import { Subtotal } from './Subtotal';
 
 interface Props {}
 
 export const Cart = (props: Props) => {
-  const productsList = useAppSelector(products);
+  const cartList = useAppSelector(products);
   const dispatch = useAppDispatch();
+  // const [subTotal, setSubTotal] = useState<number>(0);
 
   const updateHandler = (
     id: string,
@@ -41,40 +41,49 @@ export const Cart = (props: Props) => {
     );
   };
 
+  // const subTotal = useMemo(() => subTotal +  )
+  // cartList.map(
+  //   (item) => item.product && setSubTotal(subTotal + item.product?.price)
+  // );
+
   return (
     <Container>
       <Title>Shopping Cart</Title>
       <Wrapper>
         <CardWrapper>
-          {productsList.map((item: CartState) => (
-            <Card key={item.id}>
-              <ImageWrapper>
-                <Image
-                  src={require(`../../img/${item.product?.image}`)?.default}
-                  alt={item.product?.item_name}
-                />
-                <DeleteBtn onClick={() => dispatch(remove(item.id))}>
-                  Delete
-                </DeleteBtn>
-              </ImageWrapper>
-              <OptionsWrapper>
-                <CakeName>
-                  {item.product?.item_name.replaceAll('-', ' ')}
-                </CakeName>
-                <Price item={item} />
-                {item.tastes.length > 0 && (
-                  <Tastes item={item} updateHandler={updateHandler} />
-                )}
-                {item.product?.type === 'cake' && (
-                  <Size item={item} updateHandler={updateHandler} />
-                )}
-                <Quantity item={item} updateHandler={updateHandler} />
-              </OptionsWrapper>
-              {/* <button onClick={() => dispatch(remove(item.id))}>Edit</button> */}
-            </Card>
-          ))}
+          {cartList.map((item: CartState) => {
+            // item.product && setSubTotal(subTotal + item.product?.price);
+
+            return (
+              <Card key={item.id}>
+                <ImageWrapper>
+                  <Image
+                    src={require(`../../img/${item.product?.image}`)?.default}
+                    alt={item.product?.item_name}
+                  />
+                  <DeleteBtn onClick={() => dispatch(remove(item.id))}>
+                    Delete
+                  </DeleteBtn>
+                </ImageWrapper>
+                <OptionsWrapper>
+                  <CakeName>
+                    {item.product?.item_name.replaceAll('-', ' ')}
+                  </CakeName>
+                  <Price item={item} />
+                  {item.tastes.length > 0 && (
+                    <Tastes item={item} updateHandler={updateHandler} />
+                  )}
+                  {item.product?.type === 'cake' && (
+                    <Size item={item} updateHandler={updateHandler} />
+                  )}
+                  <Quantity item={item} updateHandler={updateHandler} />
+                </OptionsWrapper>
+              </Card>
+            );
+          })}
         </CardWrapper>
-        <div>Subtotal</div>
+        {/* {subTotal} */}
+        <Subtotal />
       </Wrapper>
     </Container>
   );
