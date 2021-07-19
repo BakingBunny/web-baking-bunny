38a3a0807d58ad3316fe5912a78f679a95d25cd4
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-18 23:49:53 -0600

    Edit react-hook-form

--- src/component/CheckOutPage/ClientInformation.tsx
+++ src/component/CheckOutPage/ClientInformation.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+// import { DevTool } from '@hookform/devtools';
 
 interface Props {}
 
 export const ClientInformation = (props: Props) => {
   const {
     register,
+    // control,
     handleSubmit,
     formState: { errors },
   } = useForm();
@@ -17,28 +19,28 @@ export const ClientInformation = (props: Props) => {
       <input
         type='text'
         placeholder='First name'
-        {...register('First name', { required: true, maxLength: 80 })}
+        {...register('firstName', { required: true, maxLength: 2 })}
       />
       <input
         type='text'
         placeholder='Last name'
-        {...register('Last name', { required: true, maxLength: 100 })}
+        {...register('lastName', { required: true, maxLength: 100 })}
       />
       <input
         type='text'
         placeholder='Email'
-        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
       />
       <input
         type='tel'
         placeholder='Mobile number'
-        {...register('Mobile number', {
+        {...register('mobileNumber', {
           required: true,
           minLength: 6,
           maxLength: 12,
         })}
       />
-      <textarea {...register('Allergy', {})} />
+      <textarea {...register('allergy', {})} placeholder='Allergy' />
       <input type='text' placeholder='Address' {...register('Address', {})} />
       <input
         type='text'
@@ -47,7 +49,9 @@ export const ClientInformation = (props: Props) => {
       />
       <textarea {...register('comment', {})} />
 
+      {errors.length > 0 && <div>{errors}</div>}
       <input type='submit' />
+      {/* <DevTool control={control} /> */}
     </form>
   );
 };
