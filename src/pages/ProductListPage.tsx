29162d0ef8ac5c2251f1006563eb01f3293a4491
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-22 23:42:58 -0600

    Renamed component folder

--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProductList } from '../component/ProductListPage';
+import { ProductList } from '../components/ProductListPage';
 
 interface Props {
   match: {
