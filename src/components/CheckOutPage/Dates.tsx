225c358719b34d1955dcf824d9569acc1dc92058
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-25 23:00:48 -0600

    Update react form hook

--- src/components/CheckOutPage/Dates.tsx
+++ src/components/CheckOutPage/Dates.tsx
@@ -2,31 +2,45 @@ import React from 'react';
 import { Calendar, OnChangeProps } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { checkout, update } from '../../store/checkoutSlice';
-import { useAppSelector, useAppDispatch } from '../../store/hooks';
+import { useFormContext } from 'react-hook-form';
+// import { checkout, update } from '../../store/checkoutSlice';
+// import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { DatesWrapper } from './CheckoutPageElements';
+import { OrderListInterface } from '../../interface/OrderListInterface';
 
 interface Props {}
 
 export const Dates = (props: Props) => {
-  const checkoutState = useAppSelector(checkout);
-  const dispatch = useAppDispatch();
+  // const checkoutState = useAppSelector(checkout);
+  // const dispatch = useAppDispatch();
+  const {
+    register,
+    // control,
+    watch,
+    formState: { errors },
+  } = useFormContext<OrderListInterface>();
+  // const watchPickupDeliveryDate = watch('pickupDeliveryDate', new Date());
 
   return (
     <DatesWrapper>
-      <Calendar
-        date={checkoutState.orderList.pickupDeliveryDate}
+      {/* <Calendar
+        // date={checkoutState.orderList.pickupDeliveryDate}
+        date={watchPickupDeliveryDate}
         onChange={(date: OnChangeProps) =>
-          dispatch(
-            update({
-              name: 'pickupDate',
-              value: date,
-            })
-          )
+          // dispatch(
+          //   update({
+          //     name: 'pickupDate',
+          //     value: date,
+          //   })
+          // )
+          {...register('pickupDeliveryDate', {
+            // required: 'This is required',
+            // maxLength: { value: 30, message: 'Max length exceeded' },
+          })}
         }
         // minDate={new Date().setDate(new Date().getDate() + 1)}
         minDate={new Date()}
-      />
+      /> */}
     </DatesWrapper>
   );
 };
