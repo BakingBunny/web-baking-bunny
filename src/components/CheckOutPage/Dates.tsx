682869f2d5369ce45818905a90034b3e595b9421
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-07-27 18:00:04 -0600

    Commented out checkout date to temp deploy

--- src/components/CheckOutPage/Dates.tsx
+++ src/components/CheckOutPage/Dates.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Calendar, OnChangeProps } from 'react-date-range';
+// import { Calendar, OnChangeProps } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { useFormContext } from 'react-hook-form';
+// import { useFormContext } from 'react-hook-form';
 // import { checkout, update } from '../../store/checkoutSlice';
 // import { useAppSelector, useAppDispatch } from '../../store/hooks';
 import { DatesWrapper } from './CheckoutPageElements';
-import { OrderListInterface } from '../../interface/OrderListInterface';
+// import { OrderListInterface } from '../../interface/OrderListInterface';
 
 interface Props {}
 
 export const Dates = (props: Props) => {
   // const checkoutState = useAppSelector(checkout);
   // const dispatch = useAppDispatch();
-  const {
-    register,
-    // control,
-    watch,
-    formState: { errors },
-  } = useFormContext<OrderListInterface>();
+  // const {
+  //   register,
+  //   // control,
+  //   watch,
+  //   formState: { errors },
+  // } = useFormContext<OrderListInterface>();
   // const watchPickupDeliveryDate = watch('pickupDeliveryDate', new Date());
 
   return (
