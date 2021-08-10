def6502fb65b82b93c42fb431c9606bc5c37d57e
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-08-10 17:48:55 -0600

    Design bug fixed

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -4,6 +4,8 @@ import { CheckOut } from '../components/CheckOutPage';
 interface Props {}
 
 export const CheckoutPage = (props: Props) => {
+  window.scrollTo(0, 0);
+
   return (
     <>
       <CheckOut />
--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -4,6 +4,8 @@ import Review from '../components/ReviewPage';
 interface Props {}
 
 export const ReviewPage = (props: Props) => {
+  window.scrollTo(0, 0);
+
   return (
     <>
       <Review />
