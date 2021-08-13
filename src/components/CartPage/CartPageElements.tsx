fe9486095288566e3c06ad84d57f70a81ec3a5b0
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-08-12 23:48:56 -0600

    Replaced modal product page

--- src/components/CartPage/CartPageElements.tsx
+++ src/components/CartPage/CartPageElements.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  margin: auto auto 20px;
+  margin: auto;
   height: 100%;
   max-width: 750px;
 `;
